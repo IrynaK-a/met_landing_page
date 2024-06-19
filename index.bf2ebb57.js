2ee44d84c50598546aa7885a3162f4bfe8a0a108
Iryna Konovalova <iconovalova19@gmail.com>
2024-06-19 10:33:37 +0200

    make build

---
+++ index.bf2ebb57.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.bf2ebb57.js.map
